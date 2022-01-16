2a9d9b39ccc248c1f5175e0e1ea14d3bc0825273
Samba SENE <devweb23@gmail.com>
2022-01-16 20:44:51 +0100

    cours udemy

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// class Voiture {
+
+//     constructor(marque) {
+//         this.marque = marque;
+//     }
+
+//     roule() {
+//         console.log(`roule ${this.marque} dans la route`);
+//     }
+// }
+
+// let voiture = new Voiture('bmx');
+// console.log(voiture);
+// voiture.roule( );
